8bc29b158d46ca6eb5b3d100b0324011eb5250aa
gyari <gy.gabor03@gmail.com>
2025-04-06 01:23:29 +0200

    Initial commit

---
+++ src/main/java/com/github/gyari03/LootRandomizer/chestLootRandomizer/ChestLootRandomizer.java
@@ -0,0 +1,17 @@
+package com.github.gyari03.LootRandomizer.chestLootRandomizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChestLootRandomizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
